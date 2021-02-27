9076bae9ef85ebd3dd29300425b2d0dabd13d1bd
R.Ito <kawboy442@gmail.com>
2021-02-27 17:04:36 +0900

    feat: ユーザー一覧を表示するサーブレットとJSPを設定

---
+++ src/controllers/users/EmployeesIndexServlet.java
@@ -0,0 +1,61 @@
+package controllers.users;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.User;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class EmployeesIndexServlet
+ */
+@WebServlet("/users/index")
+public class EmployeesIndexServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EmployeesIndexServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        int page = 1;
+        try{
+            page = Integer.parseInt(request.getParameter("page"));
+        } catch(NumberFormatException e) { }
+        List<User> users = em.createNamedQuery("getAllUsers", User.class)
+                                     .setFirstResult(15 * (page - 1))
+                                     .setMaxResults(15)
+                                     .getResultList();
+
+        long users_count = (long)em.createNamedQuery("getUsersCount", Long.class)
+                                       .getSingleResult();
+
+        em.close();
+
+        request.setAttribute("users", users);
+        request.setAttribute("users_count", users_count);
+        request.setAttribute("page", page);
+        if(request.getSession().getAttribute("flush") != null) {
+            request.setAttribute("flush", request.getSession().getAttribute("flush"));
+            request.getSession().removeAttribute("flush");
+        }
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/users/index.jsp");
+        rd.forward(request, response);
+    }
+
+}
