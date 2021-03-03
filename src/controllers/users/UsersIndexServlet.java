8526a32e89f45cd2039348e6c3e2e05d545efae0
R.Ito <kawboy442@gmail.com>
2021-03-03 11:50:31 +0900

    fix: インデント修正と、ファイル名変更に伴う変更が反映されていなかったのを修正

--- src/controllers/users/UsersIndexServlet.java
+++ src/controllers/users/UsersIndexServlet.java
@@ -15,47 +15,47 @@ import models.User;
 import utils.DBUtil;
 
 /**
- * Servlet implementation class EmployeesIndexServlet
+ * Servlet implementation class UsersIndexServlet
  */
 @WebServlet("/users/index")
-public class EmployeesIndexServlet extends HttpServlet {
+public class UsersIndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public EmployeesIndexServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public UsersIndexServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        EntityManager em = DBUtil.createEntityManager();
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		EntityManager em = DBUtil.createEntityManager();
 
-        int page = 1;
-        try{
-            page = Integer.parseInt(request.getParameter("page"));
-        } catch(NumberFormatException e) { }
-        List<User> users = em.createNamedQuery("getAllUsers", User.class)
-                                     .setFirstResult(15 * (page - 1))
-                                     .setMaxResults(15)
-                                     .getResultList();
+		int page = 1;
+		try{
+			page = Integer.parseInt(request.getParameter("page"));
+		} catch(NumberFormatException e) { }
+		List<User> users = em.createNamedQuery("getAllUsers", User.class)
+				.setFirstResult(15 * (page - 1))
+				.setMaxResults(15)
+				.getResultList();
 
-        long users_count = (long)em.createNamedQuery("getUsersCount", Long.class)
-                                       .getSingleResult();
+		long users_count = (long)em.createNamedQuery("getUsersCount", Long.class)
+				.getSingleResult();
 
-        em.close();
+		em.close();
 
-        request.setAttribute("users", users);
-        request.setAttribute("users_count", users_count);
-        request.setAttribute("page", page);
-        if(request.getSession().getAttribute("flush") != null) {
-            request.setAttribute("flush", request.getSession().getAttribute("flush"));
-            request.getSession().removeAttribute("flush");
-        }
+		request.setAttribute("users", users);
+		request.setAttribute("users_count", users_count);
+		request.setAttribute("page", page);
+		if(request.getSession().getAttribute("flush") != null) {
+			request.setAttribute("flush", request.getSession().getAttribute("flush"));
+			request.getSession().removeAttribute("flush");
+		}
 
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/users/index.jsp");
-        rd.forward(request, response);
-    }
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/users/index.jsp");
+		rd.forward(request, response);
+	}
 
 }
