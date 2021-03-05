d1f5ca333c7d3e07d0ee6ed674fb1f0958235639
R.Ito <kawboy442@gmail.com>
2021-03-05 10:35:13 +0900

    feat: ユーザー情報を編集するサーブレットとJSPを実装

---
+++ src/controllers/users/UsersEditServlet.java
@@ -0,0 +1,49 @@
+package controllers.users;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.User;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class UsersEditServlet
+ */
+@WebServlet("/users/edit")
+public class UsersEditServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public UsersEditServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		EntityManager em = DBUtil.createEntityManager();
+
+		User u = em.find(User.class, Integer.parseInt(request.getParameter("id")));
+
+		em.close();
+
+		request.setAttribute("user", u);
+		request.setAttribute("_token", request.getSession().getId());
+		request.getSession().setAttribute("employee_id", u.getId());
+
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/users/edit.jsp");
+		rd.forward(request, response);
+	}
+
+}
