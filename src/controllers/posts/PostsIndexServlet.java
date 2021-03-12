1280a0b7ded1cdb6e8aba2422b562e3229534741
R.Ito <kawboy442@gmail.com>
2021-03-12 11:14:14 +0900

    feat: 投稿一覧を表示するサーブレットとJSPを設定

---
+++ src/controllers/posts/PostsIndexServlet.java
@@ -0,0 +1,66 @@
+package controllers.posts;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Post;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class PostsIndexServlet
+ */
+@WebServlet("/posts/index")
+public class PostsIndexServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public PostsIndexServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		EntityManager em = DBUtil.createEntityManager();
+
+		int page;
+		try{
+			page = Integer.parseInt(request.getParameter("page"));
+		} catch(Exception e) {
+			page = 1;
+		}
+		List<Post> posts = em.createNamedQuery("getAllPosts", Post.class)
+				.setFirstResult(15 * (page - 1))
+				.setMaxResults(15)
+				.getResultList();
+
+		long posts_count = (long)em.createNamedQuery("getPostsCount", Long.class)
+				.getSingleResult();
+
+		em.close();
+
+		request.setAttribute("posts", posts);
+		request.setAttribute("posts_count", posts_count);
+		request.setAttribute("page", page);
+		if(request.getSession().getAttribute("flush") != null) {
+			request.setAttribute("flush", request.getSession().getAttribute("flush"));
+			request.getSession().removeAttribute("flush");
+		}
+
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/posts/index.jsp");
+		rd.forward(request, response);
+	}
+
+}
