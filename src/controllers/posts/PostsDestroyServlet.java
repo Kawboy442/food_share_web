e45dcd8be4c30cf86cac3683646fbc8c5f3a0aad
R.Ito <kawboy442@gmail.com>
2021-03-13 16:59:37 +0900

    chore: インデント修正

--- src/controllers/posts/PostsDestroyServlet.java
+++ src/controllers/posts/PostsDestroyServlet.java
@@ -19,32 +19,32 @@ import utils.DBUtil;
 public class PostsDestroyServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public PostsDestroyServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public PostsDestroyServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String _token = (String)request.getParameter("_token");
-        if(_token != null && _token.equals(request.getSession().getId())) {
-            EntityManager em = DBUtil.createEntityManager();
+		String _token = (String)request.getParameter("_token");
+		if(_token != null && _token.equals(request.getSession().getId())) {
+			EntityManager em = DBUtil.createEntityManager();
 
-            Post p = em.find(Post.class, Integer.parseInt(request.getParameter("id")));
-            p.setDelete_flag(1);
+			Post p = em.find(Post.class, Integer.parseInt(request.getParameter("id")));
+			p.setDelete_flag(1);
 
-            em.getTransaction().begin();
-            em.getTransaction().commit();
-            em.close();
-            request.getSession().setAttribute("flush", "削除が完了しました。");
+			em.getTransaction().begin();
+			em.getTransaction().commit();
+			em.close();
+			request.getSession().setAttribute("flush", "削除が完了しました。");
 
-            response.sendRedirect(request.getContextPath() + "/posts/index");
-        }
+			response.sendRedirect(request.getContextPath() + "/posts/index");
+		}
 	}
 
 }
