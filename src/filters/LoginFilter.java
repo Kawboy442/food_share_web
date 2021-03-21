3b6e82e95a409b2cef38a7f9c2a196e19719f0fd
R.Ito <kawboy442@gmail.com>
2021-03-21 22:15:55 +0900

    feat: imagesフォルダ下のファイルを認証処理から除外するよう設定

--- src/filters/LoginFilter.java
+++ src/filters/LoginFilter.java
@@ -42,7 +42,7 @@ public class LoginFilter implements Filter {
 		String context_path = ((HttpServletRequest)request).getContextPath();
 		String servlet_path = ((HttpServletRequest)request).getServletPath();
 
-		if(!servlet_path.matches("/css.*")) {       // CSSフォルダ内は認証処理から除外する
+		if(!servlet_path.matches("/css.*") && !servlet_path.matches("/images.*") ) {       // CSSフォルダ内は認証処理から除外する
 			HttpSession session = ((HttpServletRequest)request).getSession();
 
 			// セッションスコープに保存されたユーザー情報を取得
