7860fa81aaf63611f5a287ebc749ae2673f94109
R.Ito <kawboy442@gmail.com>
2021-03-23 23:17:10 +0900

    remove: 冗長な記述を削除

--- src/controllers/search/SearchServlet.java
+++ src/controllers/search/SearchServlet.java
@@ -28,16 +28,6 @@ public class SearchServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		String storeName = "";
-		String userName = "";
-		int evaluation = 0;
-
-		request.setAttribute("store_name", storeName);
-		request.setAttribute("user_name", userName);
-		request.setAttribute("evaluation", evaluation);
-
-
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/search/search.jsp");
 		rd.forward(request, response);
 	}
