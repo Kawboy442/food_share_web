bf6979073b4dc042c611f0ff33d7a2c6e0c91847
R.Ito <kawboy442@gmail.com>
2021-03-23 17:44:13 +0900

    feat: サーブレットからJSPを呼び出すよう設定

--- src/controllers/search/SearchServlet.java
+++ src/controllers/search/SearchServlet.java
@@ -1,6 +1,8 @@
 package controllers.search;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/search")
 public class SearchServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -27,7 +29,8 @@ public class SearchServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/search/search.jsp");
+		rd.forward(request, response);
 	}
 
 }
