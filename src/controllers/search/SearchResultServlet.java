3767aa8e08a3326e2bda12795deaecd6c7748538
R.Ito <kawboy442@gmail.com>
2021-03-23 21:01:34 +0900

    feat: JSPとの繋ぎ込みを設定

--- src/controllers/search/SearchResultServlet.java
+++ src/controllers/search/SearchResultServlet.java
@@ -1,6 +1,8 @@
 package controllers.search;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,8 +28,8 @@ public class SearchResultServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/search/result.jsp");
+		rd.forward(request, response);
 	}
 
 }
