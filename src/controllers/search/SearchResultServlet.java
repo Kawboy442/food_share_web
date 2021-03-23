a2a9673ee6ce54938838e8baa0b022309e566565
R.Ito <kawboy442@gmail.com>
2021-03-23 22:12:44 +0900

    chore: インデント修正

--- src/controllers/search/SearchResultServlet.java
+++ src/controllers/search/SearchResultServlet.java
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/search/result")
 public class SearchResultServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
