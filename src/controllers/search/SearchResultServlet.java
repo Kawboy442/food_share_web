54836f1db34f1b9cb36a90403a86814c03b9426f
R.Ito <kawboy442@gmail.com>
2021-03-23 22:47:39 +0900

    feat: 投稿一覧のサーブレットを参考に、検索結果画面のサーブレットを設定

--- src/controllers/search/SearchResultServlet.java
+++ src/controllers/search/SearchResultServlet.java
@@ -2,12 +2,15 @@ package controllers.search;
 
 import java.io.IOException;
 
+import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import utils.DBUtil;
 
 /**
  * Servlet implementation class SearchResultServlet
@@ -28,6 +31,25 @@ public class SearchResultServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		EntityManager em = DBUtil.createEntityManager();
+
+		int page;
+		try{
+			page = Integer.parseInt(request.getParameter("page"));
+		} catch(Exception e) {
+			page = 1;
+		}
+
+		em.close();
+
+		request.setAttribute("posts", posts);
+		request.setAttribute("posts_count", posts_count);
+		request.setAttribute("page", page);
+		if(request.getSession().getAttribute("flush") != null) {
+			request.setAttribute("flush", request.getSession().getAttribute("flush"));
+			request.getSession().removeAttribute("flush");
+		}
+
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/search/result.jsp");
 		rd.forward(request, response);
 	}
