9d69a1628dd6791af54c907fbe0f3feba6a35afd
R.Ito <kawboy442@gmail.com>
2021-03-23 20:55:39 +0900

    feat: 検索画面のサーブレットに必要な処理を記述

--- src/controllers/search/SearchServlet.java
+++ src/controllers/search/SearchServlet.java
@@ -28,7 +28,16 @@ public class SearchServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
+		String storeName = "";
+		String userName = "";
+		int evaluation = 0;
+
+		request.setAttribute("store_name", storeName);
+		request.setAttribute("user_name", userName);
+		request.setAttribute("evaluation", evaluation);
+
+
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/search/search.jsp");
 		rd.forward(request, response);
 	}
