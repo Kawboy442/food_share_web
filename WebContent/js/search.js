954d7caa3ec77142ea8945ab6d51042bd4b10003
R.Ito <kawboy442@gmail.com>
2021-03-29 18:00:23 +0900

    feat: 外部jsファイルを正しく動作するものに置き換え

--- WebContent/js/search.js
+++ WebContent/js/search.js
@@ -1,14 +1,26 @@
-$(function(){
-	// 1.フォーム要素の取得
-	var search_form = $("#search_form");
-	// 2.フォームのsubmitイベントを取得
-	search_form.submit(function(){
-		// 3.入力されているキーワードを取得し、正規表現により未入力かどうかを判別する
-		var storeName = $('input[id=store_name]').val();
-		var userName = $('input[id=user_name]').val();
-		var evaluation = $('input[id=evaluation]').val();
-		if(storeName.match(/^[ 　\r\n\t]*$/) && userName.match(/^[ 　\r\n\t]*$/) && evaluation == 0){
-			return false;
-		}
-	});
+
+// 各種タグの取得
+const storeName = document.querySelector("#store_name");
+const userName = document.querySelector("#user_name");
+const evaluation = document.querySelector("#evaluation");
+const button = document.querySelector("#search_btn");
+
+// 店名が入力されたらボタンを有効化
+storeName.addEventListener('keydown', () => {
+	button.disabled = false
+});
+
+// ユーザー名が入力されたらボタンを有効化
+userName.addEventListener('keydown', () => {
+	button.disabled = false
+});
+
+// 星評価で0以外が選択されたらボタンを有効化
+evaluation.addEventListener('change', function () {
+	const form = document.search_form.evaluation;
+	const select = evaluation.selectedIndex;
+	const value = form.options[select].value;
+	if(value != 0) {
+		 button.disabled = false
+	}
 });
