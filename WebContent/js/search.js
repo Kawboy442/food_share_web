d2b0855e2d76fbde266d5da61b390e97dfe8a9ca
R.Ito <kawboy442@gmail.com>
2021-04-26 11:05:04 +0900

    refactor: JavaScriptのインデントを見やすく整形

--- WebContent/js/search.js
+++ WebContent/js/search.js
@@ -1,4 +1,3 @@
-
 // 各種タグの取得
 const storeName = document.querySelector("#store_name");
 const userName = document.querySelector("#user_name");
@@ -6,21 +5,21 @@ const evaluation = document.querySelector("#evaluation");
 const button = document.querySelector("#search_btn");
 
 // 店名が入力されたらボタンを有効化
-storeName.addEventListener('keydown', () => {
-	button.disabled = false
+storeName.addEventListener("keydown", () => {
+    button.disabled = false;
 });
 
 // ユーザー名が入力されたらボタンを有効化
-userName.addEventListener('keydown', () => {
-	button.disabled = false
+userName.addEventListener("keydown", () => {
+    button.disabled = false;
 });
 
 // 星評価で0以外が選択されたらボタンを有効化
-evaluation.addEventListener('change', function () {
-	const form = document.search_form.evaluation;
-	const select = evaluation.selectedIndex;
-	const value = form.options[select].value;
-	if(value != 0) {
-		 button.disabled = false
-	}
+evaluation.addEventListener("change", function() {
+    const form = document.search_form.evaluation;
+    const select = evaluation.selectedIndex;
+    const value = form.options[select].value;
+    if (value != 0) {
+        button.disabled = false;
+    }
 });
